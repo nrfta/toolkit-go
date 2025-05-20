e608ab47616827ee876c5140c903635be9758e24
Josemar Luedke <jluedke@underline.com>
2025-05-20 10:58:52 -0700

    refactor: update tests to use dot import for ginkgo

--- must/id_test.go
+++ must/id_test.go
@@ -2,20 +2,20 @@ package must_test
 
 import (
 	"github.com/google/uuid"
-	"github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/rs/xid"
 
 	"github.com/nrfta/toolkit-go/must"
 )
 
-var _ = ginkgo.Describe("ID helpers", func() {
-	ginkgo.It("validates XID", func() {
+var _ = Describe("ID helpers", func() {
+	It("validates XID", func() {
 		Expect(must.BeXID(xid.New().String())).To(Succeed())
 		Expect(must.BeXID("invalid")).To(HaveOccurred())
 	})
 
-	ginkgo.It("validates UUID", func() {
+	It("validates UUID", func() {
 		Expect(must.BeUUID(uuid.NewString())).To(Succeed())
 		Expect(must.BeUUID("bad")).To(HaveOccurred())
 	})
--- must/suite_test.go
+++ must/suite_test.go
@@ -3,11 +3,11 @@ package must_test
 import (
 	"testing"
 
-	"github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/ginkgo/v2"
 	"github.com/onsi/gomega"
 )
 
 func TestMust(t *testing.T) {
-	gomega.RegisterFailHandler(ginkgo.Fail)
-	ginkgo.RunSpecs(t, "Must Suite")
+	gomega.RegisterFailHandler(Fail)
+	RunSpecs(t, "Must Suite")
 }
