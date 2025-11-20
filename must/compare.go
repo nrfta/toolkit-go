2854aaa1b99ae0e95f832fde84e5b906613c1d29
Josemar Luedke <jluedke@underline.com>
2025-11-20 11:54:47 -0800

    feat(must): add optional display message parameter to validation functions
    - Added variadic msg parameter to BeNonZero, BeTimeGreaterThan, BeGreaterThan, BeBetween, BeNonEmptySlice
    - When message provided, wraps error with errors.WithDisplayMessage
    - Maintains original internal error messages for debugging
    - Fully backward compatible - existing calls work without changes
    - All 17 existing tests pass

--- must/compare.go
+++ must/compare.go
@@ -8,58 +8,80 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-func BeNonZero(input any) error {
+func BeNonZero(input any, msg ...string) error {
+	var err error
+
 	switch i := input.(type) {
 	case string:
 		if i == "" {
-			return errors.InvalidArgument.Newf("must not be empty")
+			err = errors.InvalidArgument.Newf("must not be empty")
 		}
 	case *string:
 		if i == nil || *i == "" {
-			return errors.InvalidArgument.Newf("must not be empty")
+			err = errors.InvalidArgument.Newf("must not be empty")
 		}
 	case int, int16, int32, int64, int8, float32, float64:
 		if i == 0 {
-			return errors.InvalidArgument.Newf("must not be zero")
+			err = errors.InvalidArgument.Newf("must not be zero")
 		}
 	default:
 		return fmt.Errorf("must be non zero: invalid type %T", input)
 	}
 
-	return nil
+	if err != nil && len(msg) > 0 && msg[0] != "" {
+		return errors.WithDisplayMessage(err, msg[0])
+	}
+
+	return err
 }
 
-func BeTimeGreaterThan(input, low time.Time) error {
+func BeTimeGreaterThan(input, low time.Time, msg ...string) error {
 	if !input.After(low) {
-		return errors.InvalidArgument.Newf("must be after %v", low)
+		err := errors.InvalidArgument.Newf("must be after %v", low)
+		if len(msg) > 0 && msg[0] != "" {
+			return errors.WithDisplayMessage(err, msg[0])
+		}
+		return err
 	}
 
 	return nil
 }
 
-func BeGreaterThan[T constraints.Ordered](input, low T) error {
+func BeGreaterThan[T constraints.Ordered](input, low T, msg ...string) error {
 	if input <= low {
-		return errors.InvalidArgument.Newf("must be greater than %v", low)
+		err := errors.InvalidArgument.Newf("must be greater than %v", low)
+		if len(msg) > 0 && msg[0] != "" {
+			return errors.WithDisplayMessage(err, msg[0])
+		}
+		return err
 	}
 
 	return nil
 }
 
-func BeBetween[T constraints.Ordered](input, low, high T) error {
+func BeBetween[T constraints.Ordered](input, low, high T, msg ...string) error {
 	if low >= high {
 		return fmt.Errorf("must be between: low param must be less than high param")
 	}
 
 	if input < low || input > high {
-		return errors.InvalidArgument.Newf("must be between %v and %v", low, high)
+		err := errors.InvalidArgument.Newf("must be between %v and %v", low, high)
+		if len(msg) > 0 && msg[0] != "" {
+			return errors.WithDisplayMessage(err, msg[0])
+		}
+		return err
 	}
 
 	return nil
 }
 
-func BeNonEmptySlice(slice []any) error {
+func BeNonEmptySlice(slice []any, msg ...string) error {
 	if len(slice) == 0 {
-		return errors.InvalidArgument.New("slice must not be empty")
+		err := errors.InvalidArgument.New("slice must not be empty")
+		if len(msg) > 0 && msg[0] != "" {
+			return errors.WithDisplayMessage(err, msg[0])
+		}
+		return err
 	}
 
 	return nil
