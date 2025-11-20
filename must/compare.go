46ffb3cad1e8234291b6c263e47f6cf5f1594db6
Josemar Luedke <jluedke@underline.com>
2025-11-20 12:01:08 -0800

    refactor(must): rename msg parameter to displayMsg for clarity

--- must/compare.go
+++ must/compare.go
@@ -9,14 +9,14 @@ import (
 )
 
 // withDisplayMessage is a helper that optionally wraps an error with a display message
-func withDisplayMessage(err error, msg ...string) error {
-	if err != nil && len(msg) > 0 && msg[0] != "" {
-		return errors.WithDisplayMessage(err, msg[0])
+func withDisplayMessage(err error, displayMsg ...string) error {
+	if err != nil && len(displayMsg) > 0 && displayMsg[0] != "" {
+		return errors.WithDisplayMessage(err, displayMsg[0])
 	}
 	return err
 }
 
-func BeNonZero(input any, msg ...string) error {
+func BeNonZero(input any, displayMsg ...string) error {
 	var err error
 
 	switch i := input.(type) {
@@ -36,40 +36,40 @@ func BeNonZero(input any, msg ...string) error {
 		return fmt.Errorf("must be non zero: invalid type %T", input)
 	}
 
-	return withDisplayMessage(err, msg...)
+	return withDisplayMessage(err, displayMsg...)
 }
 
-func BeTimeGreaterThan(input, low time.Time, msg ...string) error {
+func BeTimeGreaterThan(input, low time.Time, displayMsg ...string) error {
 	if !input.After(low) {
-		return withDisplayMessage(errors.InvalidArgument.Newf("must be after %v", low), msg...)
+		return withDisplayMessage(errors.InvalidArgument.Newf("must be after %v", low), displayMsg...)
 	}
 
 	return nil
 }
 
-func BeGreaterThan[T constraints.Ordered](input, low T, msg ...string) error {
+func BeGreaterThan[T constraints.Ordered](input, low T, displayMsg ...string) error {
 	if input <= low {
-		return withDisplayMessage(errors.InvalidArgument.Newf("must be greater than %v", low), msg...)
+		return withDisplayMessage(errors.InvalidArgument.Newf("must be greater than %v", low), displayMsg...)
 	}
 
 	return nil
 }
 
-func BeBetween[T constraints.Ordered](input, low, high T, msg ...string) error {
+func BeBetween[T constraints.Ordered](input, low, high T, displayMsg ...string) error {
 	if low >= high {
 		return fmt.Errorf("must be between: low param must be less than high param")
 	}
 
 	if input < low || input > high {
-		return withDisplayMessage(errors.InvalidArgument.Newf("must be between %v and %v", low, high), msg...)
+		return withDisplayMessage(errors.InvalidArgument.Newf("must be between %v and %v", low, high), displayMsg...)
 	}
 
 	return nil
 }
 
-func BeNonEmptySlice(slice []any, msg ...string) error {
+func BeNonEmptySlice(slice []any, displayMsg ...string) error {
 	if len(slice) == 0 {
-		return withDisplayMessage(errors.InvalidArgument.New("slice must not be empty"), msg...)
+		return withDisplayMessage(errors.InvalidArgument.New("slice must not be empty"), displayMsg...)
 	}
 
 	return nil
--- must/id.go
+++ must/id.go
@@ -7,18 +7,18 @@ import (
 )
 
 // BeXID checks if the given string is a valid XID, returns an invalid argument error if it is not.
-func BeXID(id string, msg ...string) error {
+func BeXID(id string, displayMsg ...string) error {
 	if _, err := xid.FromString(id); err != nil {
-		return withDisplayMessage(errors.InvalidArgument.Newf("must be a valid XID"), msg...)
+		return withDisplayMessage(errors.InvalidArgument.Newf("must be a valid XID"), displayMsg...)
 	}
 
 	return nil
 }
 
 // BeUUID checks if the given string is a valid UUID, returns an invalid argument error if it is not.
-func BeUUID(id string, msg ...string) error {
+func BeUUID(id string, displayMsg ...string) error {
 	if _, err := uuid.Parse(id); err != nil {
-		return withDisplayMessage(errors.InvalidArgument.New("must be a valid UUID"), msg...)
+		return withDisplayMessage(errors.InvalidArgument.New("must be a valid UUID"), displayMsg...)
 	}
 
 	return nil
